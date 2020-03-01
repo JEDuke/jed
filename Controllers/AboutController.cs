bcd6aa7a72edf3d886d3ef8621e6e02c982668af
jeduke <jethanduke@gmail.com>
2020-03-01 13:15:39 -0600

    initial commit

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using jamesethanduke.Models;
+
+namespace jamesethanduke.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly ILogger<AboutController> _logger;
+
+        public AboutController(ILogger<AboutController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
